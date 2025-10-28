2d0908450fa417a88a1d5c586d92418371792fa0
Anand Talware <anandtalware27@gmail.com>
2025-10-28 11:11:10 +0530

    normal changes in constants and entities

--- src/nfl_game_competition/entity/artifact_entity.py
+++ src/nfl_game_competition/entity/artifact_entity.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
 
-
+"""
+DAta Ingestion
+  step 2.1: this step is after config_entity.py
+  next step 3: after this step we goes to 'src/nfl_game_competition/component/data_ingestion.py' 
+"""
 @dataclass
 class DataIngestionArtifact:
     feature_store_file_path: str
